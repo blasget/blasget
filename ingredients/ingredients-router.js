50993ae0c8cf5a7fadaaf37fc9bf727e7b1ae91b
Tobias Blasge <blasget@edu.htl-villach.at>
2025-06-22 20:02:30 +0200

    first post and get request

--- ingredients/ingredients-router.js
+++ ingredients/ingredients-router.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
 
-router.get('/ingredients', (_req, res) => res.send('Hi'));
+const ingredients = [];
+let nextId = 0;
+
+const singleIngredient = {
+    id: 1,
+    text: "",
+    isBasic: true,
+    //ofStock = []
+} 
+
+router.get('/ingredients', (_req, res) => res.json(ingredients));
+
+router.post('/ingredients', (req, res) =>{
+    const newIngredient = req.body;
+    newIngredient.id = nextId;
+    nextId++;
+    ingredients.push(newIngredient);
+    res.json(newIngredient);
+})
 
 module.exports = router
